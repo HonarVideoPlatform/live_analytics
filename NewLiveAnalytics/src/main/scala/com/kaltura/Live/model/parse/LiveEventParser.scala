2099587cde9b625130e6abe90586d281a33e0b46
HonarVideoPlatform <borhan.d@live.com>
2017-08-17 05:09:24 +0430

    v0.5.63.2

--- NewLiveAnalytics/src/main/scala/com/kaltura/Live/model/parse/LiveEventParser.scala
+++ NewLiveAnalytics/src/main/scala/com/kaltura/Live/model/parse/LiveEventParser.scala
@@ -125,8 +125,16 @@ object LiveEventParser extends Serializable with MetaLog[BaseLog]
                          if (event.bufferTime < 0)
                               event.bufferTime = 0
 
-                         if (paramsMap.contains("event:bitrate"))
-                              event.bitrate = paramsMap("event:bitrate").toLong
+                         if (paramsMap.contains("event:bitrate")) {
+                              try {
+                                   event.bitrate = paramsMap("event:bitrate").toLong
+                              } catch {
+                                   case e: Exception => {
+                                        logger.warn("event bitrate is not a numeric value, ignore bitrate data" + line)
+                                        event.bitrate = -1
+                                   }
+                              }
+                         }
 
                          if (paramsMap.contains("event:referrer"))
                               event.referrer = paramsMap("event:referrer")
